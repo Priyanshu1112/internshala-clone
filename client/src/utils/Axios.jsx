d0b8ffabc08dbee74260b7f6d42b800137f485ba
Priyanshu1112 <97700245+Priyanshu1112@users.noreply.github.com>
2023-10-20 20:45:32 +0530

    Update Axios.jsx

--- client/src/utils/Axios.jsx
+++ client/src/utils/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://internshala-clone-api.vercel.app/",
 });
 
 export default instance;
